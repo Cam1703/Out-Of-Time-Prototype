1a64134a635a55889ac45b14081a18de7434b2cc
Cam1703 <a20180103@pucp.edu.pe>
2025-06-02 20:14:07 -0500

    Zombie Damaged Animation

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,10 +4,19 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _health = 30f;
     [SerializeField] private float _damage = 10f;
-
+    private ZombieEnemyAnimation _zombieEnemyAnimation;
+    
+    public void Start()
+    {
+        _zombieEnemyAnimation = GetComponentInChildren<ZombieEnemyAnimation>();
+    }
 
     public void TakeDamage(float damage)
     {
+        if (_zombieEnemyAnimation != null)
+        {
+            _zombieEnemyAnimation.HurtAnimation(); 
+        }
         _health -= damage;
         if (_health <= 0) Destroy(gameObject);
     }
