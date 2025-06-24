b58768e1c4158c654149ac696f8ada2168654beb
Gian2560 <167040565+Gian2560@users.noreply.github.com>
2025-06-24 15:06:48 -0500

    final

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -6,7 +6,7 @@ public class Menu : MonoBehaviour
     public void PlayGame()
     {
         
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
 
     }
     
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+ï»¿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
@@ -6,10 +6,12 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private float _maxHealth = 100f;
     [SerializeField] private float _healObjetctHeal = 20f;
     [SerializeField] private float _inmunityDuration = 0.5f;
+    [SerializeField] private float _currentHealth;
+    [SerializeField] private int _healObjectQuantity;
 
-    private float _currentHealth;
+
     private float _inmunityTimer = 0f;
-    private int _healObjectQuantity;
+    //private int _healObjectQuantity;
     private bool isDead = false;
     private PlayerAnimation _playerAnimation;
 
@@ -61,26 +63,23 @@ public class PlayerHealth : MonoBehaviour
 
         _currentHealth -= damage;
 
-        // Asegúrate de que UIManager esté correctamente inicializado antes de actualizar la UI
-        if (UIManager.Instance != null)
-        {
-            UIManager.Instance.UpdateHealthText();  // Actualiza la UI con la nueva salud
-        }
+        UIManager.Instance.UpdateHealthTextWithValue(_currentHealth);  // Actualiza la UI con la nueva salud
+        //UIManager.Instance.UpdateHealth();
 
         if (_playerAnimation != null)
         {
-            _playerAnimation.HurtAnimation();  // Llamar a la animación de daño
+            _playerAnimation.HurtAnimation();  // Llamar a la animaciï¿½n de daï¿½o
         }
 
         if (_currentHealth <= 0)
         {
             _currentHealth = 0;
             isDead = true;
-            // Llamamos a la función para reiniciar el nivel
+            // Llamamos a la funciï¿½n para reiniciar el nivel
             Invoke("ResetLevel", 2f);  // Esperar 2 segundos antes de reiniciar la escena
         }
 
-        _inmunityTimer = _inmunityDuration;  // Activar inmunidad tras recibir daño
+        _inmunityTimer = _inmunityDuration;  // Activar inmunidad tras recibir daï¿½o
     }
 
     public void UseHealObject()
@@ -91,16 +90,16 @@ public class PlayerHealth : MonoBehaviour
             _healObjectQuantity--;
             if (_currentHealth > _maxHealth) _currentHealth = _maxHealth;
 
-            // Actualizamos la UI con la nueva salud
-            if (UIManager.Instance != null)
-            {
-                UIManager.Instance.UpdateHealthText();
-                UIManager.Instance.UpdateHealersText();
-            }
+            /*UIManager.Instance.UpdateHealthText();
+            UIManager.Instance.UpdateHealersText();*/
+            UIManager.Instance.UpdateHealthTextWithValue(_currentHealth);
+            UIManager.Instance.UpdateHealersTextWithValue(_healObjectQuantity);
+
+
         }
     }
 
-    // Método para cargar los datos del progreso solo cuando muere el jugador
+    // Mï¿½todo para cargar los datos del progreso solo cuando muere el jugador
     public void LoadPlayerData()
     {
         // Cargar la salud del jugador desde PlayerPrefs si ya existen datos guardados
@@ -110,7 +109,7 @@ public class PlayerHealth : MonoBehaviour
             _healObjectQuantity = PlayerPrefs.GetInt("HealObjects", 3);
         }
 
-        // Cargar la posición del jugador
+        // Cargar la posiciï¿½n del jugador
         if (PlayerPrefs.HasKey("PlayerPosX"))
         {
             float posX = PlayerPrefs.GetFloat("PlayerPosX");
@@ -118,22 +117,26 @@ public class PlayerHealth : MonoBehaviour
             float posZ = PlayerPrefs.GetFloat("PlayerPosZ");
             transform.position = new Vector3(posX, posY, posZ);
         }
-
+        Debug.Log("se prepara para carga datos");
         // Actualizamos la UI con los datos cargados
         if (UIManager.Instance != null)
         {
-            UIManager.Instance.UpdateHealthText();
-            UIManager.Instance.UpdateHealersText();
+            Debug.Log("carga datos");
+            /*UIManager.Instance.UpdateHealthText();
+            UIManager.Instance.UpdateHealersText();*/
+            UIManager.Instance.UpdateHealthTextWithValue(_currentHealth);
+            UIManager.Instance.UpdateHealersTextWithValue(_healObjectQuantity);
+            UIManager.Instance.UpdateBulletsTextWithValue(0);
         }
     }
 
-    // Método para guardar el progreso
+    // Mï¿½todo para guardar el progreso
     public void SaveProgress()
     {
         PlayerPrefs.SetFloat("PlayerHealth", _currentHealth);
         PlayerPrefs.SetInt("HealObjects", _healObjectQuantity);
 
-        // Guardar la posición del jugador
+        // Guardar la posiciï¿½n del jugador
         PlayerPrefs.SetFloat("PlayerPosX", transform.position.x);
         PlayerPrefs.SetFloat("PlayerPosY", transform.position.y);
         PlayerPrefs.SetFloat("PlayerPosZ", transform.position.z);
@@ -141,24 +144,19 @@ public class PlayerHealth : MonoBehaviour
         PlayerPrefs.Save();
     }
 
-    // Método para reiniciar el nivel
+    // Mï¿½todo para reiniciar el nivel
     public void ResetLevel()
     {
         // Llamamos a SceneManager para recargar la escena
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.name);  // Reiniciamos la misma escena
 
+
         _currentHealth = _maxHealth;  // Restaurar la salud a 100 al reiniciar
         _healObjectQuantity = 3;      // Restaurar los objetos curativos a 3
 
-        // Actualizamos la UI con los valores iniciales
-        if (UIManager.Instance != null)
-        {
-            UIManager.Instance.UpdateHealthText();
-            UIManager.Instance.UpdateHealersText();
-        }
-
-        // Cargar los datos del jugador después de que la escena se haya reiniciado
-        Invoke("LoadPlayerData", 0.8f); // Esperamos un breve momento para que la escena termine de reiniciarse antes de cargar los datos
-    }
+        /*
+        // Cargar los datos del jugador despuï¿½s de que la escena se haya reiniciado
+        Invoke("LoadPlayerData", 0.8f); // Esperamos un breve momento para que la escena termine de reiniciarse antes de cargar losÂ datos*/
+Â Â Â Â }
 }
