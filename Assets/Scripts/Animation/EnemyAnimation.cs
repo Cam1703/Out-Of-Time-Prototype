8b8bb7f227bf89f3d47a2b87c2aa8632827ce9b8
Cam1703 <a20180103@pucp.edu.pe>
2025-07-09 21:33:04 -0500

    Add enemy SFX and audio integration to prefabs and scripts
    Added new sound effect assets for enemy hurt, attack, and bullet actions. Updated enemy prefabs to include AudioSource components and linked relevant audio clips. Modified EnemyAnimation.cs to play hurt and melee attack sounds when appropriate.

--- Assets/Scripts/Animation/EnemyAnimation.cs
+++ Assets/Scripts/Animation/EnemyAnimation.cs
@@ -5,14 +5,25 @@ public class EnemyAnimation : MonoBehaviour
     private Animator _animator;
     private SpriteRenderer _sprite;
 
+    // Referencias al audio
+    private AudioSource _audioSource;
+    [SerializeField] private AudioClip _hurtSound; // Sonido que se reproduce al recibir daño.
+    [SerializeField] private AudioClip _maleeAttackSound; // Sonido que se reproduce al realizar un ataque cuerpo a cuerpo.
+
     void Start()
     {
         _animator = GetComponent<Animator>();
         _sprite = GetComponent<SpriteRenderer>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     public void HurtAnimation()
     {
+        // Reproduce el sonido de daño si está configurado.
+        if (_hurtSound != null)
+        {
+            _audioSource.PlayOneShot(_hurtSound); // Reproduce el sonido de daño.
+        }
         // Hace un parpadeo del sprite del jugador al recibir daño.
         _sprite.color = new Color(1f, 0.5f, 0.5f, 1f); // Cambia el color del sprite a rojo claro.
         Invoke("ResetSpriteColor", 0.1f); // Llama a ResetSpriteColor después de 0.1 segundos.
@@ -21,6 +32,11 @@ public class EnemyAnimation : MonoBehaviour
 
     public void MaleeAttackAnimation()
     {
+        // Reproduce el sonido del ataque cuerpo a cuerpo si está configurado.
+        if (_maleeAttackSound != null)
+        {
+            _audioSource.PlayOneShot(_maleeAttackSound); // Reproduce el sonido del ataque cuerpo a cuerpo.
+        }
         _animator.Play("Attack"); 
     }
 
